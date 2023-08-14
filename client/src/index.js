dd0dcd63cb197cb27eda910ed80e1120e912d3f4
Iryna <irka.mor.2002@gmail.com>
2023-08-14 15:26:58 +0200

    added vercel.json and config.yml

--- client/src/index.js
+++ client/src/index.js
@@ -5,30 +5,28 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLosF_YL9iFRnTNGgAbtBNz5M9Avow-wM",
+  authDomain: "expansion4you-b4046.firebaseapp.com",
+  projectId: "expansion4you-b4046",
+  storageBucket: "expansion4you-b4046.appspot.com",
+  messagingSenderId: "648825997527",
+  appId: "1:648825997527:web:1bf1b5be92b0c230d58892",
+  measurementId: "G-2XGJ3HJPP6"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
     <Router>
       <App />
     </Router>
-  //</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
-
-// import React from 'react'
-// import {createRoot} from 'react-dom/client'
-// import {BrowserRouter as Router} from 'react-router-dom'
-// import {App} from './components/App'
-
-// const container = document.getElementById('root')
-// const root = createRoot(container)
-
-// root.render(
-//     <Router>
-//         <App />
-//     </Router>
-//     )
